495226f424293577fd651c0271189353cbc35f50
Aditya Hoode <47203586+AdityaHoode@users.noreply.github.com>
2026-04-27 12:39:52 +0530

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -0,0 +1,11 @@
+# R1
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        c = [0]*26
+        for i in range(len(s)):
+            c[ord(s[i])-ord('a')]+=1
+            c[ord(t[i])-ord('a')]-=1
+        return all(j==0 for j in c)
