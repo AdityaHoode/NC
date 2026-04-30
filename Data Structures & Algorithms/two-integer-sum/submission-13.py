b5d0c3776a5d54549598206dea78734f1ae14715
Aditya Hoode <47203586+AdityaHoode@users.noreply.github.com>
2026-05-01 00:07:29 +0530

    Add: two-integer-sum - submission-13

---
+++ Data Structures & Algorithms/two-integer-sum/submission-13.py
@@ -0,0 +1,9 @@
+# R1
+from collections import defaultdict
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen={}
+        for i in range(len(nums)):
+            if target-nums[i] in seen:
+                return [seen[target-nums[i]], i]
+            seen[nums[i]]=i
