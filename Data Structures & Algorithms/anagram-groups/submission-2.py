ac85115fca8c7066576f6f4b5f010d4684a4f81b
Aditya Hoode <47203586+AdityaHoode@users.noreply.github.com>
2026-05-01 18:26:17 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,14 @@
+# R1
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d=defaultdict(list)
+        for i in range(len(strs)):
+            if d["".join(sorted(strs[i]))] is None:
+                d["".join(sorted(strs[i]))]=[]
+            d["".join(sorted(strs[i]))].append(strs[i])
+        # op=[]
+        # for k,v in d.items():
+        #     op.append(v)
+        # return op
+        return list(d.values())
