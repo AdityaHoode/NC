db388b6afa7f372b0a69a8f41078a7d3f8dc170c
Aditya Hoode <47203586+AdityaHoode@users.noreply.github.com>
2026-05-30 18:45:15 +0530

    Add: trapping-rain-water - submission-15

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-15.py
@@ -0,0 +1,14 @@
+# R1
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        stk=[]
+        area=0
+        for i,h in enumerate(height):
+            while stk and stk[-1][1]<h:
+                bottom=stk.pop()
+                if len(stk)!=0:
+                    width=i-stk[-1][0]-1
+                    height=min(stk[-1][1], h)-bottom[1]
+                    area+=height*width
+            stk.append((i,h))    
+        return area                                           
