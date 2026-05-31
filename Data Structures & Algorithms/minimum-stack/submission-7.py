02144290adba003632d4f35e645753ef19e3903a
Aditya Hoode <47203586+AdityaHoode@users.noreply.github.com>
2026-05-31 13:08:44 +0530

    Add: minimum-stack - submission-7

---
+++ Data Structures & Algorithms/minimum-stack/submission-7.py
@@ -0,0 +1,28 @@
+class MinStack:
+
+    def __init__(self):
+        self.stk=[]
+        self.min_stk=[]
+        self.min_val=0
+
+    def push(self, val: int) -> None:
+        self.stk.append(val)
+        if not self.min_stk:
+            self.min_val=val
+            self.min_stk.append(val)
+        else:
+            if self.min_val>=val:
+                self.min_val=val
+                self.min_stk.append(val)
+
+    def pop(self) -> None:
+        pop_val=self.stk.pop()
+        if pop_val==self.min_stk[-1]:
+            self.min_stk.pop()
+
+    def top(self) -> int:
+        return self.stk[-1]
+
+    def getMin(self) -> int:
+        print(self.min_stk)
+        return self.min_stk[-1]
