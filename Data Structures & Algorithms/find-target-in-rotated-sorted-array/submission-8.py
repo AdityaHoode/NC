ab8587ab14be940189e35d3dc936a410de3dc1ac
Aditya Hoode <47203586+AdityaHoode@users.noreply.github.com>
2026-06-09 00:06:10 +0530

    Add: find-target-in-rotated-sorted-array - submission-8

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-8.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findDeflectionIndex(self, nums: List[int]) -> int:
+        l,r=0,len(nums)-1
+        while l<r:
+            m=(l+r)//2
+            if nums[m]>nums[r]:
+                l=m+1
+            else:
+                r=m
+        return r
+    
+    def binary_search(self, nums: List[int], target: int, l: int, r: int) -> int:
+        while l<=r:
+            m=(l+r)//2
+            if target>nums[m]:
+                l=m+1
+            elif target<nums[m]:
+                r=m-1
+            else:
+                return m
+        return -1
+
+    def search(self, nums: List[int], target: int) -> int:
+        deflection_index=self.findDeflectionIndex(nums)
+        if target>=nums[deflection_index] and target<=nums[-1]:
+            return self.binary_search(nums, target, deflection_index, len(nums)-1)
+        else:
+            return self.binary_search(nums, target, 0, deflection_index-1)
